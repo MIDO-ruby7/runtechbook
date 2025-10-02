d92e27d9081e92d3bd0f97d68d32de55316bae6d
hisaju <hisaju@gmail.com>
2025-10-02 16:58:30 +0900

    ひさじゅ原稿追加

--- free/free.js
+++ free/free.js
@@ -18,5 +18,10 @@ module.exports = [
     path: 'free/11-mochimochifarao.md',
     title: 'AIと協働するための最低限の設計指針針',
     theme: './styles/base.css'
+  },
+  {
+    path: 'free/hisaju.md',
+    title: 'ひさじゅ式発火原因別火消し術 〜世界一役に立たないプロジェクトマネジメント術〜',
+    theme: './styles/base.css'
   }
 ];
