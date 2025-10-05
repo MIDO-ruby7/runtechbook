359f81d079de3dc16d34407e0b4c91dad3987047
massan-E <masahiro.greed@gmail.com>
2025-10-05 19:35:39 +0900

    一旦書き終わり

--- free/free.js
+++ free/free.js
@@ -9,4 +9,9 @@ module.exports = [
     title: 'Samlpe Title',
     theme: './styles/base.css'
   },
+  {
+    path: 'free/59-massan.md',
+    title: 'ActiveRecordの内部設計 ～その正体は“遅延評価”という名の高度なアーキテクチャだった～',
+    theme: './styles/base.css'
+  },
 ];
